d7b6b0760ffa4fed0bfd3cf06a70a54b7242b0fc
Deniz Sharideh <deniz.sharideh@gmail.com>
2025-06-24 06:47:19 +0200

    add autodoc paths

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project Path Configuration ----------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./..'))
+sys.path.insert(0, os.path.abspath('../pages'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
